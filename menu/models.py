fa29fb2925720b2141b7a3ca088e947ecfdb3ec7
GirumZ <girum.zebe@gmail.com>
2024-11-04 23:49:39 +0300

    [Update]: Added Category, Section, MenuItem, MenuItemIngredient models

--- menu/models.py
+++ menu/models.py
@@ -6,3 +6,40 @@ class Ingredient(models.Model):
     name = models.CharField(max_length=50)
     quantity_in_stock = models.DecimalField(max_digits=6, decimal_places=2)
     unit = models.CharField(max_length=20)
+
+    def __str__(self):
+        return(self.name)
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return(self.name)
+
+class Section(models.Model):
+    name = models.CharField(max_length=50)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return(self.name)
+
+class MenuItem(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    section = models.ForeignKey(Section, on_delete=models.SET_NULL, null=True)
+    available = models.BooleanField(default=True)
+
+    def __str__(self):
+        return(self.name)
+
+class MenuItemIngredient(models.Model):
+    menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    quantity_required = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return(self.name)
