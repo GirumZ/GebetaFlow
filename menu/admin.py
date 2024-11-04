c5430f913c6da6537b0abbc3153fe70a1e17a491
GirumZ <girum.zebe@gmail.com>
2024-11-04 23:48:18 +0300

    [Update]: Added more models for the admin site

--- menu/admin.py
+++ menu/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Ingredient
+from .models import Ingredient, MenuItem, Section, Category, MenuItemIngredient
 
 # Register your models here.
 
-admin.site.register(Ingredient)
+admin.site.register([Ingredient, MenuItem, Section, Category, MenuItemIngredient])
