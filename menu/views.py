e2b3866248c72123bb8ae0c3aaddbb79383582a1
GirumZ <girum.zebe@gmail.com>
2024-11-16 18:49:04 +0300

    [Create]: A list_item view for listing all the items in the menu

--- menu/views.py
+++ menu/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import MenuItem, Category, Section, Ingredient, MenuItemIngredient
 
 # Create your views here.
+def list_menu(request):
+    items = MenuItem.objects.prefetch_related('menuitemingredient_set__ingredient')
+    return render(request, 'menu/menu_list.html', {'items': items})
