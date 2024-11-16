74c2f8c77bcd525f29586781bc46b96b2ceec272
GirumZ <girum.zebe@gmail.com>
2024-11-16 22:41:53 +0300

    [Update]: Add IngredientType and IngredientTag models to better classify ingredients

--- menu/admin.py
+++ menu/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Ingredient, MenuItem, Section, Category, MenuItemIngredient
+from .models import Ingredient, MenuItem, Section, Category, MenuItemIngredient, IngredientType, IngredientTag
 
 # Register your models here.
 
-admin.site.register([Ingredient, MenuItem, Section, Category, MenuItemIngredient])
+admin.site.register([Ingredient, MenuItem, Section, Category, MenuItemIngredient, IngredientType, IngredientTag])
