b785d2bca36b468a61c3e5a0d96e6cc6250fe56f
GirumZ <girum.zebe@gmail.com>
2024-11-16 22:44:07 +0300

    [Update]: Added IngredientType and IngredientTag models with a clean methods for the models to prevent negative inputs

--- menu/models.py
+++ menu/models.py
@@ -1,24 +1,49 @@
+from django.core.exceptions import ValidationError
 from django.db import models
 
 # Create your models here.
 
+class IngredientType(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+    
+class IngredientTag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
 class Ingredient(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, unique=True)
+    ingredient_type = models.ForeignKey(IngredientType,  on_delete=models.SET_NULL, null=True, blank=True)
     quantity_in_stock = models.DecimalField(max_digits=10, decimal_places=2)
-    unit = models.CharField(max_length=20)
+    unit = models.CharField(max_length=20, choices=[
+        ('g', 'grams'),
+        ('ml', 'milliliters'),
+        ('pcs', 'pieces'),
+    ])
+    ingredient_tags = models.ManyToManyField(IngredientTag, blank=True)
 
     def __str__(self):
         return(self.name)
+    
+    def clean(self):
+        if self.quantity_in_stock < 0:
+            raise ValidationError("Quantity in stock can not be negative")
 
 class Category(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, unique=True)
     description = models.TextField(blank=True)
 
     def __str__(self):
         return(self.name)
 
 class Section(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, unique=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     description = models.TextField(blank=True)
 
@@ -26,7 +51,7 @@ class Section(models.Model):
         return(self.name)
 
 class MenuItem(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, unique=True)
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=5, decimal_places=2)
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
@@ -43,3 +68,7 @@ class MenuItemIngredient(models.Model):
 
     def __str__(self):
         return(self.menu_item.name + "-" + self.ingredient.name)
+    
+    def clean(self):
+        if self.quantity_required < 0:
+            raise ValidationError("Quantity required can not be negative")
