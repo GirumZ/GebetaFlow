3e88e39497cb656fdd3b16875d27645631025317
GirumZ <girum.zebe@gmail.com>
2024-12-01 22:16:51 +0300

    [Update]: Updated the urls files to handle the Django api endpoings

--- menu/urls.py
+++ menu/urls.py
@@ -1,6 +1,26 @@
-from django.urls import path
-from . import views
+from django.urls import path, include
+# from . import views
+from rest_framework.routers import DefaultRouter
+from .views import (
+    IngredientTypeViewSet, IngredientTagViewSet, IngredientViewSet, CategoryViewSet,
+    SectionViewSet, MenuItemViewSet, MenuItemIngredientViewSet
+)
 
+"""
 urlpatterns = [
     path('', views.list_menu, name='menu_list')
+]
+"""
+
+router = DefaultRouter()
+router.register('ingredient-type', IngredientTypeViewSet)
+router.register('ingredient-tag', IngredientTagViewSet)
+router.register('ingredients', IngredientViewSet)
+router.register('categories', CategoryViewSet)
+router.register('sections', SectionViewSet)
+router.register('menu-items', MenuItemViewSet)
+router.register('menu-item-ingredients', MenuItemIngredientViewSet)
+
+urlpatterns = [
+    path('menu/', include(router.urls)),
 ]
